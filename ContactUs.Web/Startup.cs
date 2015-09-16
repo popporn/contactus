7675d9f85be50c4afe3bb93bb485becda961df13
popporn <poppornk@hotmail.com>
2015-09-16 16:16:21 +0700

    Add initial Visual Studio projects
    Contains empty Models, DataAccess, Services, Facts, and ASP.NET MVC

---
+++ ContactUs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactUs.Web.Startup))]
+namespace ContactUs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
