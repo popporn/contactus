3ba622f8aca35211aa007397edca00ebe248e20e
popporn <poppornk@hotmail.com>
2015-09-16 16:33:11 +0700

    Add Ticket and TicketStatus

---
+++ ContactUs.Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactUs.Models
+{
+    public class Ticket
+    {
+        public string Id { get; set; }
+        public DateTime LastActivityDate { get; set; }
+        public string LastActivityByUser { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public TicketStatus Status { get; private set; }
+
+        public void Accept()
+        {
+
+        }
+        public void Close()
+        {
+
+        }
+        public void Reject()
+        {
+
+        }
+    }
+}
---
+++ ContactUs.Models/TicketStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContactUs.Models
+{
+    enum TicketStatus
+    {
+        New,
+        Accepted,
+        Closed
+    }
+}
